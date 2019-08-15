3e197c1d1426cd88600b487c9f0fa23eed27c28c
lautarose <49945833+lautarose@users.noreply.github.com>
2019-08-15 18:54:21 -0300

    Ejercicio 1 unidad 1

--- U01_Recursividad/Ej-01/main.cpp
+++ U01_Recursividad/Ej-01/main.cpp
@@ -3,5 +3,11 @@
 
 int main() {
     std::cout << "Ejercicio 01/01\n" << std::endl;
+    int Base,Exponente;
+    std::cout<<"Ingrese Base"<<std::endl;
+    std::cin>>Base;
+    std::cout<<"Ingrese Exponente"<<std::endl;
+    std::cin>>Exponente;
+    std::cout<<Base<<"^"<<Exponente<<"= "<<potencia(Base,Exponente);
     return 0;
 }
