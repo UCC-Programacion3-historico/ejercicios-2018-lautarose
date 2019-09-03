81ebec3c4dc8afb5c76f6abce2bf498096ad37c4
lautarose <49945833+lautarose@users.noreply.github.com>
2019-09-02 23:11:42 -0300

    ejercicio 04 unidad 01

--- U01_Recursividad/Ej-04/main.cpp
+++ U01_Recursividad/Ej-04/main.cpp
@@ -3,5 +3,6 @@
 
 int main() {
     std::cout << "Ejercicio 01/04\n" << std::endl;
+    std::cout <<mcd(2366,273);
     return 0;
 }
--- U01_Recursividad/Ej-04/mcd.cpp
+++ U01_Recursividad/Ej-04/mcd.cpp
@@ -3,5 +3,5 @@
 unsigned int mcd(unsigned int a, unsigned int b) {
     if(b==0)
         return a;
-    return
+    return mcd(b,a%b);
 }
